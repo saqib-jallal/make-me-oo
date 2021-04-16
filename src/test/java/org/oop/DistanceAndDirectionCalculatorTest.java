cd19231d5e5745ab91f3abb91add202bc8b1c0a8
saqib-jallal <saqib.jallal@thoughtworks.com>
2021-04-16 07:13:42 +0530

    Problems found with Shivam's OOP solution.

--- src/test/java/org/oop/DistanceAndDirectionCalculatorTest.java
+++ src/test/java/org/oop/DistanceAndDirectionCalculatorTest.java
@@ -11,6 +11,7 @@ public class DistanceAndDirectionCalculatorTest {
     void twoPointsWithSameXAndYCoordinatesShouldHaveADistanceOfZero() {
         Point origin = new Point(0, 0);
         Assertions.assertEquals(0, DistanceAndDirectionCalculator.distance(origin, origin));
+        // Should have compared it by creating two Point-objects with same x and y coordinates.
     }
 
     @Test
@@ -21,6 +22,7 @@ public class DistanceAndDirectionCalculatorTest {
 
         Assertions.assertEquals(1, DistanceAndDirectionCalculator.distance(origin, point1));
         Assertions.assertEquals(1, DistanceAndDirectionCalculator.distance(origin, point2));
+        // Can have a factory method for creating point on unit circle under Point-class and can receive an argument ANGLE at which the point is to be created.
     }
 
     @Test
@@ -29,16 +31,19 @@ public class DistanceAndDirectionCalculatorTest {
         Point point2 = new Point(-1, 0);
 
         Assertions.assertEquals(2, DistanceAndDirectionCalculator.distance(point1, point2));
+        // Should have created two points using createPointOnUnitCircle(factory-method) by giving angles of 180deg off.
     }
 
     @Test
-    void originAndPointOnPostiveXAxisShouldBeZeroRadiansAway() {
+    void originAndPointOnPositiveXAxisShouldBeZeroRadiansAway() {
         Point origin = new Point(0, 0);
         Point point1 = new Point(1, 0);
         Point point2 = new Point(3, 0);
 
         Assertions.assertEquals(0, direction(origin, point1));
         Assertions.assertEquals(0, direction(origin, point2));
+        // Can have a factory-method for creating a point on origin.
+        // Can have a factory-method for creating a point on x and/or y axis.
     }
 
     @Test
